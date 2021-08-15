e9e881ee3afbcc55f71803bd23effcc7ca6a7ef3
nijeabob <leehyejin0331@naver.ocm>
2021-08-15 23:52:47 +0900

    2021-08-15 nijeabob

--- app/src/main/java/com/example/had/LoginActivity.kt
+++ app/src/main/java/com/example/had/LoginActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.had.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private var emailPattern = "[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\\.[a-zA-Z]{2,3}"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,17 +24,25 @@ class LoginActivity : AppCompatActivity() {
         }
 
         binding.loginButton.setOnClickListener {
-            if(TextUtils.isEmpty(binding.idEditText.text) && TextUtils.isEmpty(binding.pwEditText.text)) {
-                Toast.makeText(this, "아이디와 비밀번호를 입력하세요.", Toast.LENGTH_SHORT).show()
+            if(TextUtils.isEmpty(binding.loginEmailEditText.text) && TextUtils.isEmpty(binding.pwEditText.text)) {
+                Toast.makeText(this, "이메일과 비밀번호를 입력하세요.", Toast.LENGTH_SHORT).show()
+             }
+            else {
+                if(binding.loginEmailEditText.text.toString().trim { it <= ' ' }.matches(emailPattern.toRegex()) && !TextUtils.isEmpty(binding.pwEditText.text)) {
+                    startActivity(Intent(this, MainActivity::class.java))
+                    binding.loginEmailEditText.setBackgroundColor(R.drawable.white_edittext)
+                }
+                else {
+                    Toast.makeText(this, "이메일 형식으로 입력하세요.", Toast.LENGTH_SHORT).show()
+                    binding.loginEmailEditText.setBackgroundResource(R.drawable.red_edittext)
+                }
+                if (TextUtils.isEmpty(binding.loginEmailEditText.text)) {
+                    Toast.makeText(this, "이메일을 입력하세요.", Toast.LENGTH_SHORT).show()
+                }
+                else if(TextUtils.isEmpty(binding.pwEditText.text)) {
+                    Toast.makeText(this, "비밀번호를 입력하세요.", Toast.LENGTH_SHORT).show()
+                }
             }
-            else if (TextUtils.isEmpty(binding.idEditText.text)) {
-                Toast.makeText(this, "아이디를 입력하세요.", Toast.LENGTH_SHORT).show()
-            }
-            else if(TextUtils.isEmpty(binding.pwEditText.text)) {
-                Toast.makeText(this, "비밀번호를 입력하세요.", Toast.LENGTH_SHORT).show()
-            }
-            else
-                startActivity(Intent(this, MainActivity::class.java))
         }
     }
 
