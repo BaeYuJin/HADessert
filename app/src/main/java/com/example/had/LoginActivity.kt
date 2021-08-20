9ba5759505ed1ee7d0223ee922bc31dcdbc916d4
khl3846 <khl3846@naver.com>
2021-08-20 19:16:41 +0900

    2021-08-20 hyori

--- app/src/main/java/com/example/had/LoginActivity.kt
+++ app/src/main/java/com/example/had/LoginActivity.kt
@@ -3,17 +3,28 @@ package com.example.had
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.had.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 
 class LoginActivity : AppCompatActivity() {
+    private val TAG = "Register"
     private var emailPattern = "[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\\.[a-zA-Z]{2,3}"
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        auth = Firebase.auth
+
+
 
         binding.signUpButton.setOnClickListener {
             startActivity(Intent(this, SignupActivity::class.java))
@@ -29,8 +40,22 @@ class LoginActivity : AppCompatActivity() {
              }
             else {
                 if(binding.loginEmailEditText.text.toString().trim { it <= ' ' }.matches(emailPattern.toRegex()) && !TextUtils.isEmpty(binding.pwEditText.text)) {
-                    startActivity(Intent(this, MainActivity::class.java))
-                    binding.loginEmailEditText.setBackgroundColor(R.drawable.white_edittext)
+
+                    auth.signInWithEmailAndPassword(binding.loginEmailEditText.text.toString().trim(), binding.pwEditText.text.toString().trim())
+                            .addOnCompleteListener(this) { task ->
+                                if (task.isSuccessful) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "signInWithEmail:success")
+                                    val user = auth.currentUser
+                                    startActivity(Intent(this, MainActivity::class.java))
+                                    binding.loginEmailEditText.setBackgroundColor(R.drawable.white_edittext)
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                                    Toast.makeText(baseContext, "없는 이메일이거나 잘못된 비밀번호입니다.",
+                                            Toast.LENGTH_SHORT).show()
+                                }
+                            }
                 }
                 else {
                     Toast.makeText(this, "이메일 형식으로 입력하세요.", Toast.LENGTH_SHORT).show()
--- app/src/main/java/com/example/had/SignupActivity.java
+++ app/src/main/java/com/example/had/SignupActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -27,9 +28,10 @@ import java.util.HashMap;
 public class SignupActivity extends AppCompatActivity {
 
     private static final String TAG = "Register";
-    EditText mEmailText, mPasswordText, mPasswordcheckText, mNameText, mIdText,  mBirthText;
-    Button mRegisterBtn, mPasswordBtn;
-    RadioButton mMaleBtn, mFemaleBtn;
+    private EditText mEmailText, mPasswordText, mPasswordcheckText, mNameText, mIdText,  mBirthText;
+    private Button mRegisterBtn, mPasswordBtn;
+    private RadioGroup mRadioGroup;
+    private String mSex;
 
     private FirebaseAuth firebaseAuth;
     @Override
@@ -46,17 +48,26 @@ public class SignupActivity extends AppCompatActivity {
         mRegisterBtn = findViewById(R.id.signupbutton);
         mNameText = findViewById(R.id.name);
         mBirthText = findViewById(R.id.birth);
-        mMaleBtn = findViewById(R.id.male_rbt);
-        mFemaleBtn = findViewById(R.id.female_rbt);
         mPasswordBtn = findViewById(R.id.pwbtn);
+        mRadioGroup = findViewById(R.id.radiogroup);
+
+        mRadioGroup.setOnCheckedChangeListener(radioGroupButtonChangeListener);
 
         mPasswordBtn.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
+                String pwd = mPasswordText.getText().toString().trim();
+                String pwdcheck = mPasswordcheckText.getText().toString().trim();
 
+                if(pwd.equals(pwdcheck)) {
+                    Toast.makeText(SignupActivity.this, "비밀번호가 일치합니다.", Toast.LENGTH_SHORT).show();
+                } else{
+                    Toast.makeText(SignupActivity.this, "비밀번호가 틀렸습니다. 다시 입력해 주세요.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+
 
         mRegisterBtn.setOnClickListener(new View.OnClickListener(){
 
@@ -64,8 +75,8 @@ public class SignupActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 //가입 정보 가져오기//
-                final String email = mEmailText.getText().toString().trim();
-                //final String uid = mIdText.getText().toString().trim();
+                String email = mEmailText.getText().toString().trim();
+                //String uid = mIdText.getText().toString().trim();
                 String pwd = mPasswordText.getText().toString().trim();
                 String pwdcheck = mPasswordcheckText.getText().toString().trim();
 
@@ -76,26 +87,30 @@ public class SignupActivity extends AppCompatActivity {
                     mDialog.setMessage("가입중입니다...");
                     mDialog.show();
 
+
                     //파이어베이스에 신규계정 등록하기
                     firebaseAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
-
                             //가입 성공시
                             if (task.isSuccessful()) {
                                 mDialog.dismiss();
 
                                 FirebaseUser user = firebaseAuth.getCurrentUser();
                                 String email = user.getEmail();
-                                String uid = user.getUid();
+                                String uid = mIdText.getText().toString().trim();
                                 String name = mNameText.getText().toString().trim();
+                                String birth = mBirthText.getText().toString().trim();
+                                String sex = mSex;
 
                                 //해쉬맵 테이블을 파이어베이스 데이터베이스에 저장
-                                HashMap<Object,String> hashMap = new HashMap<>();
+                                HashMap<Object, String> hashMap = new HashMap<>();
 
-                                hashMap.put("uid",uid);
-                                hashMap.put("email",email);
-                                hashMap.put("name",name);
+                                hashMap.put("uid", uid);
+                                hashMap.put("email", email);
+                                hashMap.put("name", name);
+                                hashMap.put("birth", birth);
+                                hashMap.put("sex", sex);
 
 
                                 FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -108,14 +123,11 @@ public class SignupActivity extends AppCompatActivity {
                                 startActivity(intent);
                                 finish();
                                 Toast.makeText(SignupActivity.this, "회원가입에 성공하셨습니다.", Toast.LENGTH_SHORT).show();
-
                             } else {
                                 mDialog.dismiss();
-                                Toast.makeText(SignupActivity.this, "이미 존재하는 아이디 입니다.", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(SignupActivity.this, "회원가입에 실패하셨습니다.", Toast.LENGTH_SHORT).show();
                                 return;  //해당 메소드 진행을 멈추고 빠져나감.
-
                             }
-
                         }
                     });
 
@@ -129,4 +141,15 @@ public class SignupActivity extends AppCompatActivity {
         });
 
     }
+    RadioGroup.OnCheckedChangeListener radioGroupButtonChangeListener=new RadioGroup.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            if(checkedId == R.id.male_rbt){
+                mSex = "male";
+            } else{
+                mSex = "female";
+            }
+        }
+    };
+
 }
