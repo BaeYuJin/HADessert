e74cedf775ccce42d44ffa4fe9ed250159044b3d
khl3846 <khl3846@naver.com>
2021-08-22 19:52:51 +0900

    2021-08-22 hyori

---
+++ app/src/main/java/com/example/had/PreferenceUtil.kt
@@ -0,0 +1,49 @@
+package com.example.had
+
+import android.content.Context
+import android.content.SharedPreferences
+
+/*class PreferenceUtil(context: Context) {
+    private val prefs: SharedPreferences = context.getSharedPreferences("my_id", Context.MODE_PRIVATE)
+
+    fun getString(key: String, defValue: String): String {
+        return prefs.getString(key, defValue).toString()
+    }
+    fun setString(key: String, str: String) {
+        prefs.edit().putString(key, str).apply()
+    }
+}*/
+
+object PreferenceUtil {
+    private val MY_ACCOUNT : String = "account"
+
+    fun setUserId(context: Context, input: String) {
+        val prefs : SharedPreferences = context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
+        val editor : SharedPreferences.Editor = prefs.edit()
+        editor.putString("MY_ID", input)
+        editor.commit()
+    }
+
+    fun getUserId(context: Context): String {
+        val prefs : SharedPreferences = context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
+        return prefs.getString("MY_ID", "").toString()
+    }
+
+    fun setAutoLogin(context: Context, input:String){
+        val prefs : SharedPreferences = context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
+        val editor : SharedPreferences.Editor = prefs.edit()
+        editor.putString("MY_LOGIN", input)
+        editor.commit()
+    }
+    fun getAutoLogin(context: Context): String {
+        val prefs : SharedPreferences = context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
+        return prefs.getString("MY_LOGIN", "").toString()
+    }
+
+    fun clearUser(context: Context) {
+        val prefs : SharedPreferences = context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
+        val editor : SharedPreferences.Editor = prefs.edit()
+        editor.clear()
+        editor.commit()
+    }
+}
