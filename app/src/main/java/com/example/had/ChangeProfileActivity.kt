5dc1633658087c1478ff35d38cae6589c5867b87
yrchoo <yeahrin0000@hansung.ac.kr>
2021-08-27 18:24:29 +0900

    yrchoo

--- app/src/main/java/com/example/had/ChangeProfileActivity.kt
+++ app/src/main/java/com/example/had/ChangeProfileActivity.kt
@@ -1,24 +1,20 @@
 package com.example.had
 
-import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
 import com.example.had.databinding.ActivityChangeProfileBinding
 import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import com.theartofdev.edmodo.cropper.CropImage
+import com.theartofdev.edmodo.cropper.CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE
+import com.theartofdev.edmodo.cropper.CropImage.getActivityResult
 
 
 class ChangeProfileActivity : AppCompatActivity() {
     private lateinit var binding: ActivityChangeProfileBinding
-    private val OPEN_GALLERY = 1
     private val TAG = this.javaClass.simpleName
-
-    private val getContent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-        result: ActivityResult ->binding.NewProfileImage.setImageURI(result.data?.data)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,24 +22,25 @@ class ChangeProfileActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.getImageB.setOnClickListener {
-            val intent = Intent(android.provider.MediaStore.Images.Media.CONTENT_TYPE)
-            intent.setType("image/*")
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = MediaStore.Images.Media.CONTENT_TYPE
+            intent.type = "image/*"
+            intent.putExtra("crop", true)
             launcher.launch(intent)
         }
     }
 
     var launcher = registerForActivityResult(
         StartActivityForResult()
-    ) { result ->
+    ) {
+        result ->
         if (result.resultCode == RESULT_OK) {
             Log.e(TAG, "result : $result")
             val intent = result.data
             Log.e(TAG, "intent : $intent")
             val uri = intent!!.data
             Log.e(TAG, "uri : $uri")
-            binding.NewProfileImage.setImageURI(uri);
-
+            binding.NewProfileImage.setImageURI(uri)
         }
     }
-
 }
