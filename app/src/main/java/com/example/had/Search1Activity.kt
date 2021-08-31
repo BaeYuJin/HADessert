a25a8634296112b45e711d15fa62387e4a3c0bda
보근 유 <qhrms97@naver.com>
2021-08-31 16:26:01 +0900

    2021-08-31 BaeYuJin

--- app/src/main/java/com/example/had/Search1Activity.kt
+++ app/src/main/java/com/example/had/Search1Activity.kt
@@ -6,9 +6,11 @@ import android.util.Log
 import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.had.databinding.ActivitySearch1Binding
+import com.example.had.databinding.ActivitySearchBinding
 
 class Search1Activity : AppCompatActivity() {
     private lateinit var binding: ActivitySearch1Binding
+    private lateinit var binding2: ActivitySearchBinding
     val list = ArrayList<DataSearch>()
     val list2 = ArrayList<DataSearch>()
     private val adapter2 = RecyclerAdapterRecent(list2)
@@ -20,13 +22,23 @@ class Search1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySearch1Binding.inflate(layoutInflater)
+        binding2 = ActivitySearchBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        var intent = Intent(this, SearchActivity::class.java)
 
         binding.mainSearchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String): Boolean {
                 recentWord = query
                 list2.add(DataSearch(recentWord!!))
                 listRecent(list2)
+                /*var intent = Intent(this, SearchActivity::class.java)
+                intent.putExtra("searchword", binding.mainSearchView.toString())*/
+                //var query2 = binding.mainSearchView.query.toString()
+                Log.d("query:", query)
+                intent.putExtra("word", query)
+                //binding2.textView4.text = query
+                startActivity(intent)
                 return true
             }
 
@@ -38,6 +50,9 @@ class Search1Activity : AppCompatActivity() {
             startActivity(Intent(this, SearchActivity::class.java))
         }
 
+        var query = binding.mainSearchView.query
+        //binding.mainSearchView.setQuery(binding.mainSearchView.query, true)
+
 
         list.add(DataSearch("마카롱"))
         list.add(DataSearch("치즈케이크"))
@@ -46,6 +61,7 @@ class Search1Activity : AppCompatActivity() {
 
         //Log.e("sList", sList.toString())
         binding.RecentRv.adapter = adapter2
+
     }
 
     fun listRecent(AddList : ArrayList<DataSearch>){
