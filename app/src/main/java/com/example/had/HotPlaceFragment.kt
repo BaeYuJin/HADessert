e5319d2a292eb85c1802d259901992dc82ac2950
khl3846 <khl3846@naver.com>
2021-09-01 15:31:12 +0900

    2021-09-01 hyori

---
+++ app/src/main/java/com/example/had/HotPlaceFragment.kt
@@ -0,0 +1,19 @@
+package com.example.had
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class HotPlaceFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_hot_place, null)
+        return view
+    }
+}
