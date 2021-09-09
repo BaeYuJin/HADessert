d0528b0cd3ea2230aa85f0940eb5d1b124c6f39d
yrchoo <yeahrin0000@hansung.ac.kr>
2021-09-09 15:12:47 +0900

    2021-09-09 yrchoo

--- app/src/main/java/com/example/had/fragment/SetLocationNowFragment.kt
+++ app/src/main/java/com/example/had/fragment/SetLocationNowFragment.kt
@@ -13,9 +13,5 @@ class SetLocationNowFragment : Fragment() {
         super.onCreate(savedInstanceState)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-    }
+
 }
