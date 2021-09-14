ebd3dc9a57340e9758e3629c1c72503b7b9cb815
yrchoo <yeahrin0000@hansung.ac.kr>
2021-09-14 19:20:21 +0900

    2021-09-14-please final yrchoo

--- app/src/main/java/com/example/had/FireStorageViewModel.kt
+++ app/src/main/java/com/example/had/FireStorageViewModel.kt
@@ -5,6 +5,7 @@ import android.graphics.BitmapFactory
 import android.provider.MediaStore.Images.Media.getBitmap
 import android.widget.ImageView
 import androidx.appcompat.content.res.AppCompatResources.getDrawable
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -18,18 +19,18 @@ class FireStorageViewModel : ViewModel() {
     val storageRef = storage.reference
     val imageRefChild = storageRef.child("profileImages/${user?.uid}.jpg")
     val imageRefUrl = storage.getReferenceFromUrl("gs://hadessert-c6192.appspot.com/profileImages/${user?.uid}.jpg")
-    lateinit var result : Bitmap
+    var result = MutableLiveData<Bitmap>()
 
     fun getImageRef(){
         imageRefUrl?.getBytes(Long.MAX_VALUE)?.addOnSuccessListener {
             val bmp = BitmapFactory.decodeByteArray(it, 0, it.size)
-            result = bmp
+            result.value = bmp
         }?.addOnFailureListener {
             // Failed to download the image
         }
     }
 
     public fun setImage(view: ImageView){
-        view?.setImageBitmap(result)
+        view?.setImageBitmap(result.value)
     }
 }
