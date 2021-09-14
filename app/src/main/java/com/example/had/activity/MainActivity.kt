52bfb3d75f46179993abbd414b57fbbf12308c91
nijeabob <leehyejin0331@naver.com>
2021-09-15 04:53:38 +0900

    2021-09-15 nijeabob

--- app/src/main/java/com/example/had/activity/MainActivity.kt
+++ app/src/main/java/com/example/had/activity/MainActivity.kt
@@ -199,30 +199,64 @@ class MainActivity : AppCompatActivity() {
     private fun initRecycler() {
         recyclerAdapterStar = RecyclerAdapterStar(this)
         binding.starRv.adapter = recyclerAdapterStar
-        var shopName : String =""
-        var address : String =""
-        var phone : String =""
 
         if (user != null) {
             db.collection(user.uid).document("성북동빵공장").get().addOnSuccessListener { documentSnapshot ->
-                val star = documentSnapshot.toObject<StarData>()
+                var star = documentSnapshot.toObject<StarData>()
                 if (star != null) {
-                    shopName = star.name
-                    address = star.address
-                    phone = star.phone
                     Log.d("test", star.name)
                     Log.d("test", star.address)
                     Log.d("test", star.phone)
+
+                    starlist.apply {
+                        add(StarData(img = R.drawable.cake, name = star.name , address = star.address , phone = star.phone))
+                        recyclerAdapterStar.starlist = starlist
+                        recyclerAdapterStar.notifyDataSetChanged()
+                    }
                 }
             }
-        }
+            db.collection(user.uid).document("블랑제메종북악").get().addOnSuccessListener { documentSnapshot ->
+                var star2 = documentSnapshot.toObject<StarData>()
+                if (star2 != null) {
+                    Log.d("test", star2.name)
+                    Log.d("test", star2.address)
+                    Log.d("test", star2.phone)
 
-        starlist.apply {
-            //add(StarData(img = R.drawable.bread, shop = "이름", address = "주소", tel = "010"))
-            add(StarData(img = R.drawable.bread, name = "${shopName}", address = address , phone = phone))
+                    starlist.apply {
+                        add(StarData(img = R.drawable.cake, name = star2.name , address = star2.address , phone = star2.phone))
+                        recyclerAdapterStar.starlist = starlist
+                        recyclerAdapterStar.notifyDataSetChanged()
+                    }
+                }
+            }
+            db.collection(user.uid).document("수연산방").get().addOnSuccessListener { documentSnapshot ->
+                var star3 = documentSnapshot.toObject<StarData>()
+                if (star3 != null) {
+                    Log.d("test", star3.name)
+                    Log.d("test", star3.address)
+                    Log.d("test", star3.phone)
 
-            recyclerAdapterStar.starlist = starlist
-            recyclerAdapterStar.notifyDataSetChanged()
+                    starlist.apply {
+                        add(StarData(img = R.drawable.bread, name = star3.name , address = star3.address , phone = star3.phone))
+                        recyclerAdapterStar.starlist = starlist
+                        recyclerAdapterStar.notifyDataSetChanged()
+                    }
+                }
+            }
+            db.collection(user.uid).document("해로커피").get().addOnSuccessListener { documentSnapshot ->
+                var star4 = documentSnapshot.toObject<StarData>()
+                if (star4 != null) {
+                    Log.d("test", star4.name)
+                    Log.d("test", star4.address)
+                    Log.d("test", star4.phone)
+
+                    starlist.apply {
+                        add(StarData(img = R.drawable.coffee, name = star4.name , address = star4.address , phone = star4.phone))
+                        recyclerAdapterStar.starlist = starlist
+                        recyclerAdapterStar.notifyDataSetChanged()
+                    }
+                }
+            }
         }
     }
 
